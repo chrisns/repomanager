b38e52946fb330c184e9d286d3c71c1d3fb8624b
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-11-27 00:05:23 +0000

    linting

--- handler.js
+++ handler.js
@@ -82,9 +82,13 @@ const applyConfig = async repo => {
     })
   }
 
-  if (repo.desiredConfig.branchProtection && repo.private === false && repo.default_branch) {
-    const branchProtectionConfig = await Promise.all(repo.desiredConfig.branchProtection.map(
-      async a => {
+  if (
+    repo.desiredConfig.branchProtection &&
+    repo.private === false &&
+    repo.default_branch
+  ) {
+    const branchProtectionConfig = await Promise.all(
+      repo.desiredConfig.branchProtection.map(async a => {
         return {
           owner: repo.owner.login,
           repo: repo.name,
@@ -92,21 +96,34 @@ const applyConfig = async repo => {
           branch:
             a.branch === '__DEFALT_BRANCH__' ? repo.default_branch : a.branch,
           required_status_checks:
-            a.required_status_checks.contexts === "ALL" ? await (async () => {
-              try {
-                a.required_status_checks.contexts = Array.from(new Set((await octokit.checks.listForRef({
-                  owner: repo.owner.login,
-                  repo: repo.name,
-                  ref: `refs/heads/${a.branch === '__DEFALT_BRANCH__' ? repo.default_branch : a.branch}`,
-                })).data.check_runs.map(check => check.name)))
-              } catch (error) {
-                a.required_status_checks.contexts = []
-              }
-              return a.required_status_checks
-            })(a.required_status_checks) : a.required_status_checks,
+            a.required_status_checks.contexts === 'ALL'
+              ? await (async () => {
+                  try {
+                    a.required_status_checks.contexts = Array.from(
+                      new Set(
+                        (
+                          await octokit.checks.listForRef({
+                            owner: repo.owner.login,
+                            repo: repo.name,
+                            ref: `refs/heads/${
+                              a.branch === '__DEFALT_BRANCH__'
+                                ? repo.default_branch
+                                : a.branch
+                            }`
+                          })
+                        ).data.check_runs.map(check => check.name)
+                      )
+                    )
+                  } catch (error) {
+                    a.required_status_checks.contexts = []
+                  }
+                  return a.required_status_checks
+                })(a.required_status_checks)
+              : a.required_status_checks
         }
-      }
-    ))
+      })
+    )
+    console.log(branchProtectionConfig[0])
     await Promise.all(
       branchProtectionConfig.map(octokit.repos.updateBranchProtection)
     )
