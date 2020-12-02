742f0a1dabe39289f91d5e04c04072b8b255b362
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-12-02 10:54:06 +0000

    linting fixes

--- handler.js
+++ handler.js
@@ -98,30 +98,27 @@ const applyConfig = async repo => {
           required_status_checks:
             a.required_status_checks.contexts === 'ALL'
               ? await (async () => {
-                try {
-                  a.required_status_checks.contexts = Array.from(
-                    new Set(
-                      (
-                        await octokit.checks.listForRef({
-                          owner: repo.owner.login,
-                          repo: repo.name,
-                          ref: `refs/heads/${a.branch === '__DEFALT_BRANCH__'
-                            ? repo.default_branch
-                            : a.branch
+                  try {
+                    a.required_status_checks.contexts = Array.from(
+                      new Set(
+                        (
+                          await octokit.checks.listForRef({
+                            owner: repo.owner.login,
+                            repo: repo.name,
+                            ref: `refs/heads/${
+                              a.branch === '__DEFALT_BRANCH__'
+                                ? repo.default_branch
+                                : a.branch
                             }`
-                        })
-                      ).data.check_runs
-                        .map(check => check.name)
-
-
-
-                    )
-                  ).filter(name => name !== '.github/dependabot.yml')
-                } catch (error) {
-                  a.required_status_checks.contexts = []
-                }
-                return a.required_status_checks
-              })(a.required_status_checks)
+                          })
+                        ).data.check_runs.map(check => check.name)
+                      )
+                    ).filter(name => name !== '.github/dependabot.yml')
+                  } catch (error) {
+                    a.required_status_checks.contexts = []
+                  }
+                  return a.required_status_checks
+                })(a.required_status_checks)
               : a.required_status_checks
         }
       })
